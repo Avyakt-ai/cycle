24dcc123facef1513de7120eeefa4801f27250ef
avyakt <avyakt.seven@gmail.com>
2025-02-19 16:40:48 +0530

    added models to admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Cycle, Lock, Ride
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Cycle)
+admin.site.register(Lock)
+admin.site.register(Ride)
