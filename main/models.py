75e2127ba675aa03d604375eac3deb9774343edf
avyakt <avyakt.seven@gmail.com>
2025-02-26 15:09:24 +0530

    changed from AbstractUser to UserProfile

--- main/models.py
+++ main/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import User
 
-class User(AbstractUser):
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
     is_active_rider = models.BooleanField(default=False)
     wallet_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
 
-    groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
 
     def __str__(self):
-        return self.username
+        return self.user.username
 
 
 class Cycle(models.Model):
@@ -48,9 +47,8 @@ class Lock(models.Model):
 
 
 
-
 class Ride(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     cycle = models.ForeignKey(Cycle, on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
